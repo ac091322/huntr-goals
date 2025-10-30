7c4d57ee4424759ae21f7a1455b9558216734392
Alan Chang <alanc091322@gmail.com>
2025-10-29 18:17:07 -0700

    fixed double goal #2 and window size

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import { Builder } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
 import { huntrGoals } from "./tests/huntr_goals.e2e.js";
 
 
 (async function main() {
-    const driver = await new Builder().forBrowser("chrome").build();
+    const options = new chrome.Options();
+    options.windowSize({ width: 1350, height: 9999 });
+
+    const driver = await new Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(options)
+        .build();
 
     try {
         await huntrGoals(driver);
--- tests/huntr_goals.e2e.js
+++ tests/huntr_goals.e2e.js
@@ -37,7 +37,7 @@ async function huntrGoals(driver) {
 
     console.log("💃 Just sit back as it's doing its thing... 🕺");
 
-    const goalsArray = ["Universal Goal #1", "Universal Goal #2", "Universal Goal #2", "Universal Goal #3", "Universal Goal #4", "Universal Goal #5", "Universal Goal #6", "Universal Goal #7", "Universal Goal #8"];
+    const goalsArray = ["Universal Goal #1", "Universal Goal #2", "Universal Goal #3", "Universal Goal #4", "Universal Goal #5", "Universal Goal #6", "Universal Goal #7", "Universal Goal #8"];
 
     for (let goal of goalsArray) {
         const currentGoalButtonXpath = `//div[contains(@class, "UserGoalBox__BoxSection-sc-otbafz-2")][.//p[contains(text(), "${goal}")]]//div[contains(@class, "AddButtonSquare-sc-n85sy6-0")]`;
